8c09eff8e098c3f785a365c3d46d9c1e38310452
Jack Tubbenhauer <hello@jacktubbenhauer.com>
2022-12-05 13:48:58 +1100

    clean up 01

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -1,12 +1,29 @@
-if __name__ == '__main__':
-    with open('data.txt') as data:
-        lines = data.read().splitlines()
+import heapq
 
+
+def parse_data(x):
+    with open(x) as data:
+        return data.read().splitlines()
+
+
+def get_three_largest(x):
+    return heapq.nlargest(3, x)
+
+
+def get_most_calories(x):
+    most_calories = 0
+    for elf_total in x:
+        if elf_total > most_calories:
+            most_calories = elf_total
+    return most_calories
+
+
+def calculate_elf_calories(x):
     current_index = 0
     elf_list = []
     first_value = True
 
-    for line in lines:
+    for line in x:
         if line:
             if first_value:
                 elf_list.append(int(line))
@@ -16,11 +33,12 @@ if __name__ == '__main__':
         else:
             current_index += 1
             first_value = True
+    return elf_list
 
-    current_highest = 0
 
-    for elf_total in elf_list:
-        if elf_total > current_highest:
-            current_highest = elf_total
+if __name__ == '__main__':
+    elf_list = calculate_elf_calories(parse_data('data.txt'))
 
-    print(current_highest)
+    print(get_most_calories(elf_list))
+
+    print(sum(get_three_largest(elf_list)))
